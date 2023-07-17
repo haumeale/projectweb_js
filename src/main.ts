560f62d2475ed2bc2b91025d9e311801b267b52c
haumeale <haumealesya@gmail.com>
2023-07-17 21:14:05 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,54 @@
 import './style.css'
 // alert('hello')
 // console.log('hello')
-let name = 'John'
-let admin = name
-console.log(admin)
+// let name = 'John'
+// let admin = name
+// console.log(admin)
+
+// 1.
+// let a = parseInt(prompt('Введите число')as string)
+// alert(a ** 2)
+
+// 2.
+// let x = parseFloat(prompt('Введите число') as string)
+// let y = parseFloat(prompt('Введите число') as string)
+// alert((x + y) / 2)
+
+// 3.
+// let a = parseFloat(prompt('Введите длину квадрата') as string)
+// alert(a * a)
+
+// 4.
+// let a = parseFloat(prompt('Введите длину в киломентрах') as string)
+// alert(a * 0.621371)
+
+// 5.
+// let x = parseFloat(prompt('Введите x') as string)
+// let y = parseFloat(prompt('Введите y') as string)
+// alert(x + y)
+// alert(x - y)
+// alert(x * y)
+// alert(x / y)
+
+// 6.
+// let a =parseFloat(prompt('Enter number a') as string)
+// let b =parseFloat(prompt('Enter number b') as string)
+// alert((-b)/a)
+
+// 7.
+// let a = parseFloat(prompt('Enter hours') as string)
+// let b = parseFloat(prompt('Enter minutes') as string)
+// alert()
+
+// 8.
+// let a = parseFloat(prompt('Введите трёх значное число') as string)
+// a = Math.floor(a / 10) % 10
+// alert(a)
+
+
+
+
+
+
+
+
